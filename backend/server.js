8198bec88ea279a4f6471884aad3120b317ceeb0
amaan3091 <amaan.work96@gmail.com>
2024-09-11 15:33:48 +0530

    updated port to 5001

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,7 @@ app.use(express.json());
 app.use("/api/users", require("./routes/userRoutes"));
 
 // Start server
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });

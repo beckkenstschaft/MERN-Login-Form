098eb82c1e4994f92af9480622e75187a158b697
amaan3091 <amaan.work96@gmail.com>
2024-09-11 15:46:06 +0530

    updated port

--- frontend/src/Login.js
+++ frontend/src/Login.js
@@ -11,7 +11,7 @@ const Login = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const res = await axios.post("http://localhost:5000/api/users/login", {
+      const res = await axios.post("http://localhost:5001/api/users/login", {
         username,
         password,
       });
